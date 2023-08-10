3b1cff9795cf4b82da8702aaa9b2e41b7124ddd6
Andrew Chernuha <actimel333@gmail.com>
2023-08-10 18:07:22 +0300

    Adds env file

--- src/lib/useWeatherServiceHook.ts
+++ src/lib/useWeatherServiceHook.ts
@@ -2,14 +2,15 @@ import { useState } from 'react';
 import { ICityWeatherData } from '../components/game/types';
 
 const useWeatherServiceHook = (setError: React.Dispatch<React.SetStateAction<string | null>>) => {
-  const apiKey: string = 'b355db3a8217ae32abe405b8021b20a5';
   const [isLoading, setIsLoading] = useState(false);
 
   const getWeather = async (cityName: string): Promise<ICityWeatherData | null> => {
     try {
       setIsLoading(true);
 
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`);
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_WEATER}`,
+      );
       const data = await response.json();
 
       // Uncomment next line to imitate slow connection
